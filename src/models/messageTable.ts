3e14e53f646999928f12097ab75ffa3559f5e369
Jessie France <FranceJessieJ@gmail.com>
2022-08-09 15:39:13 -0600

    refactor: remove console logs

--- src/models/messageTable.ts
+++ src/models/messageTable.ts
@@ -4,9 +4,7 @@ class MessageTable {
   static getMessages() {
     return new Promise((resolve, reject) => {
       pool.query(`SELECT * FROM message`, (error: any, response: any) => {
-        if (error) console.log(error);
         if (error) return reject(error);
-        console.log('inside table function', response.rows);
         return resolve({ messages: response.rows });
       });
     });
