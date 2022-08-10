48d8a808000852905c4c30ce4d22ac9a26f1a126
Jessie France <FranceJessieJ@gmail.com>
2022-08-10 14:33:09 -0600

    feat: use email scheduler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import { app, PORT } from './app';
+import { app, PORT, SENDGRID_KEY } from './app';
+import EmailSchedule from './helpers/scheduleMail';
 
+// Start the email scheduler.
+const emailSchedule = new EmailSchedule(String(SENDGRID_KEY));
+emailSchedule.start();
+
+// Start the Express application.
 app.listen(PORT || 8888, () => {
   console.log(`Server running on port ${PORT || 8888}`);
 });
