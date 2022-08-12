000404021ed073e19d29a6ffd467ac6a21b4cc32
Jessie France <FranceJessieJ@gmail.com>
2022-08-12 17:20:16 -0600

    feat: sanitize and validate fields

--- src/routes/account/index.ts
+++ src/routes/account/index.ts
@@ -1,9 +1,27 @@
 import express from 'express';
+import { body } from 'express-validator';
 import { signup } from '../../controllers';
 import { lowerCaseEmail } from '../../middleware';
+import validation from '../../middleware/validation';
 
 const router = express.Router();
 
-router.post('/signup', lowerCaseEmail, signup);
+router.post(
+  '/signup',
+  [
+    body('email')
+      .isLength({ max: 30 })
+      .isEmail()
+      .escape()
+      .withMessage('Invalid email'),
+    body('password')
+      .isLength({ min: 2, max: 20 })
+      .escape()
+      .withMessage('Invalid password'),
+  ],
+  validation,
+  lowerCaseEmail,
+  signup
+);
 
 export { router as accountRouter };
